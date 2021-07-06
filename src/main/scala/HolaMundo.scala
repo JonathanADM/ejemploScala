25eecfd9a047b07e04ee0b3529678fea52c1fc1c
Jonathan Diaz <jonaalbert.adm@gmail.com>
2021-07-06 17:35:30 -0400

    Fin

--- src/main/scala/HolaMundo.scala
+++ src/main/scala/HolaMundo.scala
@@ -1,50 +1,46 @@
-import java.io.{BufferedReader, FileReader}
+import java.io.{BufferedReader, File, FileNotFoundException, FileReader}
 import java.util.stream
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
-object HolaMundo{
-    def main(args: Array[String]): Unit = {
-      var filename = "src/main/scala/wordlist.txt"
-      var filename2 = "D:\\Users\\Jonaa\\Documents\\INTEC\\Trimestre#8\\po\\src\\main\\scala\\WD.txt"
- /*     var beta = ReadFiles(filename2)
-      MapFiller(beta.lines())*/
-      /*GetPath(args)*/
-      val start = System.nanoTime()
-      GetPath(filename)
-      Print()
-      val end = System.nanoTime()
-      println(s"Time taken: ${(end - start) / 1000 / 1000 /1000} s")
-    }
+object HolaMundo {
+  def main(args: Array[String]): Unit = {
+    var file = new File("src/main/scala/wordlist.txt")
 
-    def ReadFiles(filename: String): BufferedReader ={
-      val br = new BufferedReader(new FileReader(filename))
-      return br
-    }
-  val AnagramSet : scala.collection.mutable.Map[String,ListBuffer[String]] = scala.collection.mutable.Map[String,ListBuffer[String]]()
-
-  def GetPath(filename: String): Unit = {
-      if(filename == null) throw new IllegalArgumentException
-      for(line <- Source.fromFile(filename).getLines) {
-        MapFill(line,AnagramSet)
-      }
+    val start = System.nanoTime()
+    GetPath(file)
+    Print()
+    val end = System.nanoTime()
+    println(s"Time taken: ${(end - start) / 1000 / 1000 / 1000} s")
   }
 
-  def MapFill(line: String, AnagramSet: scala.collection.mutable.Map[String,ListBuffer[String]] = scala.collection.mutable.Map[String,ListBuffer[String]]()): Unit ={
-     val new_line = line.sorted
-      if(AnagramSet.contains(new_line)) {
-        if(!AnagramSet(new_line).contains(line)){
-          AnagramSet(new_line) += line
-        }
-      } else{
-        AnagramSet += (new_line -> ListBuffer[String]())
+  val AnagramSet: scala.collection.mutable.Map[String, ListBuffer[String]] = scala.collection.mutable.Map[String, ListBuffer[String]]()
+
+  def GetPath(filename: File): Unit = {
+    if (filename == null) throw new IllegalArgumentException
+    if (filename.exists()) {
+      for (line <- Source.fromFile(filename).getLines) {
+        MapFill(line, AnagramSet)
+      }
+    }
+    else throw new FileNotFoundException(s"Can't find needed resource: $filename")
+  }
+
+  def MapFill(line: String, AnagramSet: scala.collection.mutable.Map[String, ListBuffer[String]] = scala.collection.mutable.Map[String, ListBuffer[String]]()): Unit = {
+    val new_line = line.sorted
+    if (AnagramSet.contains(new_line)) {
+      if (!AnagramSet(new_line).contains(line)) {
         AnagramSet(new_line) += line
       }
+    } else {
+      AnagramSet += (new_line -> ListBuffer[String]())
+      AnagramSet(new_line) += line
+    }
   }
 
-  def Print(): Unit ={
+  def Print(): Unit = {
     var sets = 0
-    for((key,value) <- AnagramSet) {
+    for ((key, value) <- AnagramSet) {
       if (value.length > 1) {
         println(value.mkString("->"))
         sets += 1
@@ -52,42 +48,4 @@ object HolaMundo{
     }
     println("Cantidad de sets: " + sets)
   }
-
-  /*def MapFiller(file: java.util.stream.Stream[String]): Unit ={
-    val A : scala.collection.mutable.Map[String,ListBuffer[String]] = scala.collection.mutable.Map[String,ListBuffer[String]]()
-
-    for (line <- file) {
-      val new_line = line.toLowerCase.sorted
-
-      if(A.contains(new_line)) A(new_line) += line
-      else{
-        A += (new_line -> ListBuffer[String]())
-        A(new_line) += line
-      }
-    }
-    for((key,value) <- A) {
-      if (value.length > 1) println(value.mkString("->"))
-    }
-  }*/
 }
-
-
-
-// www.tutorialspoint.com/scala/scala_maps.html
-/*object BufferedReader2 extends App {
-
-
-  val filename = "C:\\Users\\Miguel\\IdeaProjects\\po\\src\\main\\scala\\wordlist.txt"
-
-  val br = new BufferedReader(new FileReader(filename))
-  return br.lines()
- /* var newlineCount = 0L
-  var line = ""
-  while ({line = br.readLine; line != null}) {
-    newlineCount += 1
-  }
-  br.close
-  println(newlineCount)*/*/
-
-/*
-}*/
--- src/test/scala/HolaMundoTest.scala
+++ src/test/scala/HolaMundoTest.scala
@@ -1,12 +1,22 @@
 import org.scalatest.FunSuite
 
+import java.io.{File, FileNotFoundException}
 import scala.collection.mutable.ListBuffer
+import scala.io.Source
+
 class HolaMundoTest extends FunSuite{
   test(testName = "HolaMundo_GetPath_ArgsInput_IsNull_ThrowsNullArgumentException"){
       assertThrows[IllegalArgumentException]{
         HolaMundo.GetPath(null)
       }
   }
+  test(testName = "HolaMundo_GetPath_ArgsInput_FileNotExists_ThrowsNotFoundException"){
+    var file = new File("hola")
+    assertThrows[FileNotFoundException]{
+      HolaMundo.GetPath(file)
+    }
+  }
+
   test(testName = "HolaMundo_MapFill_MapsShouldBeEquals"){
     val Anagrams = Map("moor" -> ListBuffer("romo", "moro"), "ahlo" -> ListBuffer("hola","olah"))
     val expected : scala.collection.mutable.Map[String,ListBuffer[String]] = scala.collection.mutable.Map[String,ListBuffer[String]]()
@@ -28,6 +38,6 @@ class HolaMundoTest extends FunSuite{
     HolaMundo.MapFill("olah",expected)
     HolaMundo.MapFill("moro",expected)
 
-    assert(expected == Anagrams)
+    assert(expected != Anagrams)
   }
 }
